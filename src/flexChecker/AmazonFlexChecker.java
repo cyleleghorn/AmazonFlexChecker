8ab87838b7385e40605d066e8d37ac3317a64038
cyleleghorn <cyleleghorn@gmail.com>
2016-11-18 11:02:27 -0500

    Added project files
    Added the Eclipse project files and required libraries.

---
+++ src/flexChecker/AmazonFlexChecker.java
@@ -0,0 +1,43 @@
+package flexChecker;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AmazonFlexChecker {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AmazonFlexChecker window = new AmazonFlexChecker();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AmazonFlexChecker() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
